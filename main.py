72124466fe9e31bdba2e58837f1beed69a183555
Juan Felipe Correa Ríos <78551261+flipgamership@users.noreply.github.com>
2023-04-07 14:38:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('hola mundo esto es pithon')
+
+print("hola soy felipe y tengo 21 años")
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+# la identacion debe ser sin espacion para que cada ejecucion por lo cual si no tienes buena identacion puede fallar
+print(8 / 2)
